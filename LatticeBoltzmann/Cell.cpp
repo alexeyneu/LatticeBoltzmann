5452397debf483d6c36250df1ee5fdd408da055f
alex-PC\alex <alexneudatchin@gmail.com>
2018-07-16 22:04:31 +0400

    no message

--- LatticeBoltzmann/Cell.cpp
+++ LatticeBoltzmann/Cell.cpp
@@ -15,9 +15,8 @@ namespace LatticeBoltzmann {
 	std::array<signed char, 9> Cell::ey = std::array<signed char, 9>{ {0, 1, 1, 0, -1, -1, -1,  0,  1} };
 
 	std::array<double, 9> Cell::coeff = std::array<double, 9>{ { c0, c1, c2, c1, c2, c1, c2, c1, c2 } };
-
 	Cell::Cell()
-		: density{ {0, 0, 0, 0, 0, 0, 0, 0, 0} }
+		: density(std::array<double, 9>{ {0, 0, 0, 0, 0, 0, 0, 0, 0} })
 	{
 	}
 
